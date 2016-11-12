b67e1f801030a75db63a20f49080b19605cc916d
Daniel Nieto <danielnieto89@gmail.com>
2016-11-12 00:17:28 -0600

    add a component for each page of the app (to be used with the router)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,16 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 import { ShellComponent } from './shell/shell.component';
 import { ContentComponent } from './content/content.component';
 import { FeedComponent } from './feed/feed.component';
+import { QrcodesComponent } from './pages/qrcodes/qrcodes.component';
+import { PreferencesComponent } from './pages/preferences/preferences.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { TicketsComponent } from './pages/tickets/tickets.component';
+import { PromotionsComponent } from './pages/promotions/promotions.component';
+import { ChargesComponent } from './pages/charges/charges.component';
+import { BranchesComponent } from './pages/branches/branches.component';
+
 
 @NgModule({
     declarations: [
@@ -17,7 +27,16 @@ import { FeedComponent } from './feed/feed.component';
         SidenavComponent,
         ShellComponent,
         ContentComponent,
-        FeedComponent
+        FeedComponent,
+        QrcodesComponent,
+        PreferencesComponent,
+        HomeComponent,
+        StatisticsComponent,
+        ProductsComponent,
+        TicketsComponent,
+        PromotionsComponent,
+        ChargesComponent,
+        BranchesComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/pages/qrcodes/qrcodes.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { QrcodesComponent } from './qrcodes.component';
+
+describe('QrcodesComponent', () => {
+  let component: QrcodesComponent;
+  let fixture: ComponentFixture<QrcodesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QrcodesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QrcodesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
