4fb336310ec1024e467fd58d64577fb0a2e35f9d
Daniel Nieto <danielnieto89@gmail.com>
2017-05-06 01:19:58 -0500

    add ResponsiveService with utility functions

---
+++ src/app/responsive.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/startWith';
+
+@Injectable()
+export class ResponsiveService {
+
+    resize$: Observable<Event>;
+    MAX_SCREEN_WIDTH = 1192;
+
+    constructor() {
+        //create a stream from window's resize event
+        this.resize$ = Observable.fromEvent(window, "resize");
+    }
+
+    // returns an observable which is the current width of the screen
+    screenWidth(): Observable<number> {
+        return this.resize$
+            .map(() => this.getScreenWidth())
+            .distinctUntilChanged();
+    }
+
+    // get maximum (supported) screen width
+    getMaxScreenWidth(): number {
+        return this.MAX_SCREEN_WIDTH;
+    }
+
+    // returns the current document width
+    getScreenWidth(): number {
+        return window.document.documentElement.clientWidth;
+    }
+
+}
