946e8a58000c20d1f63c14297efea753d31ee8ba
Daniel Nieto <danielnieto89@gmail.com>
2017-06-20 19:12:52 -0500

    basic layout for charges component

--- src/app/pages/charges/charges.component.ts
+++ src/app/pages/charges/charges.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Charge} from '../../charge';
+import {ChargeStatus} from '../../charge-status';
+
+import {ChargeService} from '../../charge.service';
+
 @Component({
   selector: 'app-charges',
   templateUrl: './charges.component.html',
-  styleUrls: ['./charges.component.css', '../../content/page-title.scss']
+  styleUrls: ['./charges.component.css', '../../content/page-title.scss'],
+  providers: [ChargeService]
 })
 export class ChargesComponent implements OnInit {
 
-  constructor() { }
+  charges:Charge[];
+
+  constructor(private chargeService: ChargeService) {
+
+  }
 
   ngOnInit() {
+      this.charges = this.chargeService.getCharges();
   }
+
+  onToggleCollapse(charge:Charge){
+      charge.collapsed = !charge.collapsed;
+  }
+
+  statusText(charge):string{
+
+      let text: string;
+
+      switch(charge.status){
+          case ChargeStatus.deposited:
+            text = "depositado"
+            break;
+          case ChargeStatus.paid:
+            text = "pagado"
+            break;
+          case ChargeStatus.declined:
+            text = "declinado"
+            break;
+          case ChargeStatus.fraudulent:
+            text = "fraudulento"
+            break;
+          case ChargeStatus.refunded:
+            text = "reembolsado"
+            break;
+          case ChargeStatus.frozen:
+            text = "congelado"
+            break;
+      }
+
+      return text;
+  }
+
+
 
 }
