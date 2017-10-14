1703863a1be4b22a444143b2c93e7739998dfbd8
Daniel Nieto <danielnieto89@gmail.com>
2017-10-14 16:08:32 -0500

    toggle sidenav on "toggler" click, using ResponsiveService

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import {ResponsiveService} from '../responsive.service';
 
 @Component({
     selector: 'app-sidenav',
@@ -19,13 +20,14 @@ export class SidenavComponent implements OnInit {
 
     activeNavItem = this.navItems[0];
 
-    constructor() { }
+    constructor(private responsiveService: ResponsiveService) { }
 
     ngOnInit() {
     }
 
     toggleNav() {
         this.toggleNavEvent.emit();
+        this.responsiveService.navIsCollapsed$.next(!this.responsiveService.navIsCollapsed$.getValue());
     }
 
     onClickNavItem(navItem:any){
