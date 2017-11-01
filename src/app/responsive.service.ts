7b3c8cb5a73b8475a8e93cf7257451ecddf24df7
Daniel Nieto <danielnieto89@gmail.com>
2017-11-01 10:50:20 -0600

    use responsive.service to manage the behavior of the feed component

--- src/app/responsive.service.ts
+++ src/app/responsive.service.ts
@@ -9,50 +9,70 @@ import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class ResponsiveService {
+  resize$: Observable<Event>;
+  MAX_SCREEN_WIDTH = 986;
+  private navIsCollapsed$: BehaviorSubject<boolean> = new BehaviorSubject(true);
+  private feedIsCollapsed$: BehaviorSubject<boolean> = new BehaviorSubject(true);
 
-    resize$: Observable<Event>;
-    MAX_SCREEN_WIDTH = 986;
-    private navIsCollapsed$: BehaviorSubject<boolean> = new BehaviorSubject(true);
-
-    constructor() {
-        //create a stream from window's resize event
+  constructor() {
+    //create a stream from window's resize event
         this.resize$ = Observable.fromEvent(window, "resize").startWith(this.getScreenWidth());
-    }
+  }
 
-    // returns an observable which is the current width of the screen
-    screenWidth(): Observable<number> {
+  // returns an observable which is the current width of the screen
+  screenWidth(): Observable<number> {
         return this.resize$
             .map(() => this.getScreenWidth())
             .distinctUntilChanged();
-    }
+  }
 
-    // get maximum (supported) screen width
-    getMaxScreenWidth(): number {
-        return this.MAX_SCREEN_WIDTH;
-    }
+  // get maximum (supported) screen width
+  getMaxScreenWidth(): number {
+    return this.MAX_SCREEN_WIDTH;
+  }
 
-    // returns the current document width
-    getScreenWidth(): number {
-        return window.document.documentElement.clientWidth;
-    }
+  // returns the current document width
+  getScreenWidth(): number {
+    return window.document.documentElement.clientWidth;
+  }
 
-    collapseNav(): void{
-        this.navIsCollapsed$.next(true);
-    }
+  collapseNav(): void {
+    this.navIsCollapsed$.next(true);
+  }
 
-    expandNav(): void{
-        this.navIsCollapsed$.next(false);
-    }
+  expandNav(): void {
+    this.navIsCollapsed$.next(false);
+  }
 
-    toggleNav(): void{
-        if(this.navIsCollapsed$.getValue() === true){
-            this.expandNav();
-        } else {
-            this.collapseNav();
-        }
+  toggleNav(): void {
+    if (this.navIsCollapsed$.getValue() === true) {
+      this.expandNav();
+    } else {
+      this.collapseNav();
     }
+  }
 
-    get navIsCollapsed():Boolean{
-        return this.navIsCollapsed$.getValue();
+  collapseFeed(): void {
+    this.feedIsCollapsed$.next(true);
+  }
+
+  expandFeed(): void {
+    this.feedIsCollapsed$.next(false);
+  }
+
+  toggleFeed(): void {
+    if (this.feedIsCollapsed$.getValue() === true) {
+      this.expandFeed();
+    } else {
+      this.collapseFeed();
     }
+  }
+
+  get navIsCollapsed(): Boolean {
+    return this.navIsCollapsed$.getValue();
+  }
+
+  get feedIsCollapsed(): Boolean {
+    return this.feedIsCollapsed$.getValue();
+  }
 }
