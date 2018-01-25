e697e9caadd6e09357a54b75509a72f7431e28fa
Daniel Nieto <danielnieto89@gmail.com>
2018-01-25 16:52:49 -0600

    convert shell.component and app.component css files into scss

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -4,7 +4,7 @@ import {ResponsiveService} from '../responsive.service';
 @Component({
   selector: "app-shell",
   templateUrl: "./shell.component.html",
-  styleUrls: ["./shell.component.css"]
+  styleUrls: ["./shell.component.scss"]
 })
 export class ShellComponent implements OnInit {
 
