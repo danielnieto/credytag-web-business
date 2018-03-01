669e2c795fc7d0536ae252e38e7985925b6a1efd
Daniel Nieto <danielnieto89@gmail.com>
2018-02-28 19:18:25 -0600

    update "ChargeStatus" enum to use string instead of numbers

--- src/app/charge-status.ts
+++ src/app/charge-status.ts
@@ -1,8 +1,8 @@
 export enum ChargeStatus{
-    deposited = 1,
-    paid = 2,
-    declined = 3,
-    fraudulent = 4,
-    refunded = 5,
-    frozen = 6
+    deposited = 'deposited',
+    paid = 'paid',
+    declined = 'declined',
+    fraudulent = 'fraudulent',
+    refunded = 'refunded',
+    frozen = 'frozen'
 }
