a91edbae13c75beb9fdfb3187671b2dca88fcd95
Daniel Nieto <danielnieto89@gmail.com>
2018-03-21 20:40:18 -0600

    implement basic qrcodes component
    - with mobile styles
    - dummy data

--- src/app/pages/qrcodes/qrcodes.component.ts
+++ src/app/pages/qrcodes/qrcodes.component.ts
@@ -1,15 +1,96 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-qrcodes',
-  templateUrl: './qrcodes.component.html',
-  styleUrls: ['./qrcodes.component.scss', '../../content/page-title.scss']
+    selector: 'app-qrcodes',
+    templateUrl: './qrcodes.component.html',
+    styleUrls: ['./qrcodes.component.scss', '../../content/page-title.scss']
 })
 export class QrcodesComponent implements OnInit {
 
-  constructor() { }
+    qrCodes = [
+        {
+            name: "caja 1",
+            qrId: "#ASD32D",
+            phone: "(33) 3312 4567",
+            enabled: true
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: true
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: false
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: true
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: true
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: true
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: false
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: true
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: true
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: false
+        },
+        {
+            name: "caja 2",
+            qrId: "#HK8SNS",
+            phone: "(33) 1234 5678",
+            enabled: true
+        },
+        {
+            name: "caja 3",
+            qrId: "#ASD32D",
+            phone: "(33) 3312 4567",
+            enabled: false
+        },
+        {
+            name: "caja 1",
+            qrId: "#ASD32D",
+            phone: "(33) 3312 4567",
+            enabled: true
+        }
+    ];
 
-  ngOnInit() {
-  }
+
+    constructor() {}
+
+    ngOnInit() {}
 
 }
