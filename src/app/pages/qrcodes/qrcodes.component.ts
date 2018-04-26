b659701b9611142eb4315bbcd3e66d8e60fa03ca
Daniel Nieto <danielnieto89@gmail.com>
2018-04-25 19:44:08 -0500

    change all " to '

--- src/app/pages/qrcodes/qrcodes.component.ts
+++ src/app/pages/qrcodes/qrcodes.component.ts
@@ -13,81 +13,81 @@ export class QrcodesComponent implements OnInit {
 
     qrCodes = [
         {
-            name: "caja 1",
-            qrId: "#ASD32D",
-            phone: "(33) 3312 4567",
+            name: 'caja 1',
+            qrId: '#ASD32D',
+            phone: '(33) 3312 4567',
             enabled: true
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: true
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: false
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: true
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: true
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: true
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: false
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: true
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: true
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: false
         },
         {
-            name: "caja 2",
-            qrId: "#HK8SNS",
-            phone: "(33) 1234 5678",
+            name: 'caja 2',
+            qrId: '#HK8SNS',
+            phone: '(33) 1234 5678',
             enabled: true
         },
         {
-            name: "caja 3",
-            qrId: "#ASD32D",
-            phone: "(33) 3312 4567",
+            name: 'caja 3',
+            qrId: '#ASD32D',
+            phone: '(33) 3312 4567',
             enabled: false
         },
         {
-            name: "caja 1",
-            qrId: "#ASD32D",
-            phone: "(33) 3312 4567",
+            name: 'caja 1',
+            qrId: '#ASD32D',
+            phone: '(33) 3312 4567',
             enabled: true
         }
     ];
