20cefc0a5677ae1d996f69f137bb2a5ff50aed22
Daniel Nieto <danielnieto89@gmail.com>
2018-07-02 20:14:13 -0500

    add all fields for each tabs for settings page, and validate them

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -7,7 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+  companyForm: FormGroup;
+  billingForm: FormGroup;
+  bankForm: FormGroup;
+
+  
+  constructor() { 
+
+    this.userForm = new FormGroup({
+      fullName: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, [Validators.required])
+    });
+
+    this.companyForm = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      phone: new FormControl(null, [Validators.required]),
+      website: new FormControl(),
+      email: new FormControl(null, [Validators.email]),
+      type: new FormControl(),
+      billingInformation: new FormControl(null, [Validators.required]),
+    });
+    
+    this.billingForm = new FormGroup({
+      personType: new FormControl(null, [Validators.required]),
+      name: new FormControl(null, [Validators.required]),
+      rfc: new FormControl(null, [Validators.required]),
+      zipCode: new FormControl(null, [Validators.email]),
+      usage: new FormControl(null, [Validators.required])
+    });
+
+    this.bankForm = new FormGroup({
+      bank: new FormControl(null, [Validators.required]),
+      spei: new FormControl(null, [Validators.required]),
+      beneficiary: new FormControl(null, [Validators.required])
+    });
+
+  }
 
   ngOnInit() {
   }
