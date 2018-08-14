c449200c18b34b433b1b581de393c28f804d895c
Daniel Nieto <danielnieto89@gmail.com>
2018-08-13 20:38:35 -0500

    refactor QRCodes service to use session data for all requests

--- src/app/qrcodes.service.ts
+++ src/app/qrcodes.service.ts
@@ -1,24 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { QrCode } from './qrCode';
+import { SessionService } from './session.service';
 
 @Injectable()
 export class QrcodesService {
 
-    headers: HttpHeaders;
+    jsonHeaders = new HttpHeaders({
+        'Content-Type': 'application/json'
+    });
     endpoint = 'https://credytag-backend-dev.herokuapp.com/api/v1';
-    business = '9817834a-f9fe-4246-bff3-c7014cb7cee8';
-    branch = '7e1e1ba8-321b-46b4-be14-c9a987ca3b45';
-    token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjUwNTVhN2FlLTI4OTUtNGJ' +
-        'mNC05ZmU5LTMzYzdmYzQyOTZiYyIsImV4cCI6MTg3NjM4MTMzN30.ZLlLfp6NWfQl3HYxi2Dm1InvCZfsuKXZQI-eM1OLmyE';
+    business: string;
+    branch: string;
 
-    constructor(private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient, private session: SessionService) {
 
-        this.headers = new HttpHeaders({
-            'Authorization': this.token,
-            'Content-Type': 'application/json'
-        });
+        this.business = this.session.business;
+        this.branch = this.session.branch;
 
     }
 
@@ -32,27 +30,12 @@ export class QrcodesService {
             }
         };
 
-        const promise = new Promise((resolve, reject) => {
+        return this.httpClient.post(`${this.endpoint}/business/${this.business}/branch/${this.branch}/code`, payload, {
+            headers: this.jsonHeaders
+        }).toPromise();
 
-            this.httpClient.post(`${this.endpoint}/business/${this.business}/branch/${this.branch}/code`, payload, {
-                headers: this.headers,
-                responseType: 'text'
-            }).subscribe((response: any) => {
+    }
 
-                resolve(response);
-
-            }, (error: any) => {
-
-                reject(error);
-
-            });
-        });
-
-
-        return promise;
-
-	}
-    
     updateQr(editQrData: { id: string, name: string, phone: string, description: string, readonly qr: string }): any {
 
         const payload = {
@@ -63,24 +46,9 @@ export class QrcodesService {
             }
         };
 
-        const promise = new Promise((resolve, reject) => {
-
-            this.httpClient.put(`${this.endpoint}/business/${this.business}/branch/${this.branch}/code/${editQrData.id}`, payload, {
-                headers: this.headers,
-                responseType: 'text'
-            }).subscribe((response: any) => {
-
-                resolve(response);
-
-            }, (error: any) => {
-
-                reject(error);
-
-            });
-        });
-
-
-        return promise;
+        return this.httpClient.put(`${this.endpoint}/business/${this.business}/branch/${this.branch}/code/${editQrData.id}`, payload, {
+            headers: this.jsonHeaders
+        }).toPromise();
 
     }
 
@@ -89,7 +57,7 @@ export class QrcodesService {
         const promise = new Promise<QrCode[]>((resolve, reject) => {
 
             this.httpClient.get(`${this.endpoint}/business/${this.business}/branch/${this.branch}/code`, {
-                headers: this.headers
+                headers: this.jsonHeaders
             }).subscribe((response: any) => {
 
                 const codes: QrCode[] = [];
